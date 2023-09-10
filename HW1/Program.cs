1e63c19f0c6bc6adfa7e6876396239fa69469197
Chucchich <139654741+Chucchich@users.noreply.github.com>
2023-09-10 23:09:25 +0700

    Add files via upload

---
+++ HW1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thanh Bình\t"+"12422003\t"+"12422TN\t"+"Chucchich\t"+"morninggaugaugau@gmail.com");
+    }
+}
+
+
+
+
