5b27fe6ef7f904936e4acf19021ace8644373ac9
Bình Nguyễn <139654741+Chucchich@users.noreply.github.com>
2023-10-14 07:27:46 +0700

    Add files via upload

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,156 +1,179 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
-class Employee
+namespace HW3
 {
-    public string Name { get; set; }
-
-    public Employee(string name)
+    // Lớp Bank (Ngân hàng)
+    class Bank
     {
-        Name = name;
-    }
-}
+        private List<Account> accounts;
 
-class Item
-{
-    public string Name { get; set; }
-    public double Price { get; set; }
-    public double Discount { get; set; }
-
-    public Item(string name, double price, double discount)
-    {
-        Name = name;
-        Price = price;
-        Discount = discount;
-    }
-}
-
-class BillLine
-{
-    public Item Item { get; set; }
-    public int Quantity { get; set; }
-
-    public BillLine(Item item, int quantity)
-    {
-        Item = item;
-        Quantity = quantity;
-    }
-}
-
-class GroceryBill
-{
-    private Employee Clerk { get; set; }
-    protected List<BillLine> BillLines { get; set; }
-
-    public GroceryBill(Employee clerk)
-    {
-        Clerk = clerk;
-        BillLines = new List<BillLine>();
-    }
-
-    public void Add(BillLine billLine)
-    {
-        BillLines.Add(billLine);
-    }
-
-    public double GetTotal()
-    {
-        double total = 0.0;
-        foreach (var billLine in BillLines)
+        public Bank()
         {
-            total += (billLine.Item.Price - billLine.Item.Discount) * billLine.Quantity;
+            accounts = new List<Account>();
         }
-        return total;
-    }
 
-    public void PrintReceipt()
-    {
-        Console.WriteLine($"Hóa đơn cho nhân viên: {Clerk.Name}");
-        foreach (var billLine in BillLines)
+        public void OpenAccount(string accountNumber, string accountHolderName, string identificationNumber, decimal initialBalance, decimal interestRate)
         {
-            Console.WriteLine($"{billLine.Item.Name} x{billLine.Quantity}: ${billLine.Item.Price * billLine.Quantity}");
+            Account account = new Account(accountNumber, accountHolderName, identificationNumber, initialBalance, interestRate);
+            accounts.Add(account);
         }
-        Console.WriteLine($"Tổng: ${GetTotal()}");
-    }
-}
 
-class DiscountBill : GroceryBill
-{
-    private bool IsPreferredCustomer { get; set; }
-
-    public DiscountBill(Employee clerk, bool preferred) : base(clerk)
-    {
-        IsPreferredCustomer = preferred;
-    }
-
-    public int GetDiscountCount()
-    {
-        int count = 0;
-        foreach (var billLine in BillLines)
+        public void Deposit(string accountNumber, decimal amount, DateTime transactionDate)
         {
-            if (billLine.Item.Discount > 0)
+            Account account = FindAccount(accountNumber);
+            if (account != null)
             {
-                count += billLine.Quantity;
+                account.Deposit(amount, transactionDate);
+            }
+            else
+            {
+                Console.WriteLine("Tài khoản không tồn tại.");
             }
         }
-        return count;
-    }
 
-    public double GetDiscountAmount()
-    {
-        double amount = 0.0;
-        foreach (var billLine in BillLines)
+        public void Withdraw(string accountNumber, decimal amount, DateTime transactionDate)
         {
-            amount += billLine.Item.Discount * billLine.Quantity;
+            Account account = FindAccount(accountNumber);
+            if (account != null)
+            {
+                account.Withdraw(amount, transactionDate);
+            }
+            else
+            {
+                Console.WriteLine("Tài khoản không tồn tại.");
+            }
         }
-        return amount;
-    }
 
-    public double GetDiscountPercent()
-    {
-        if (IsPreferredCustomer)
+        public void CalculateInterest()
         {
-            double originalTotal = base.GetTotal();
-            double discountedTotal = originalTotal - GetDiscountAmount();
-            return (GetDiscountAmount() / originalTotal) * 100.0;
+            foreach (Account account in accounts)
+            {
+                account.CalculateInterest();
+            }
         }
-        return 0.0;
+
+        public void PrintReport()
+        {
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine("Số hiệu tài khoản: " + account.AccountNumber);
+                Console.WriteLine("Số tiền hiện có: " + account.Balance + " Euros");
+                Console.WriteLine("Các giao dịch:");
+                foreach (Transaction transaction in account.Transactions)
+                {
+                    Console.WriteLine("- Ngày: " + transaction.TransactionDate.ToString("dd/MM/yyyy") + ", Kiểu: " + transaction.TransactionType + ", Số tiền: " + transaction.Amount + " Euros");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private Account FindAccount(string accountNumber)
+        {
+            foreach (Account account in accounts)
+            {
+                if (account.AccountNumber == accountNumber)
+                {
+                    return account;
+                }
+            }
+            return null;
+        }
     }
-}
 
-class Program
-{
-    static void Main(string[] args)
+    // Lớp Account (Tài khoản)
+    class Account
     {
-        Console.InputEncoding = Encoding.UTF8;
-        Console.OutputEncoding = Encoding.UTF8;
-        Employee clerk = new Employee("John");
-        Item item1 = new Item("Candy Bar", 1.35, 0.25);
-        Item item2 = new Item("Soda", 2.0, 0.0);
-        Item item3 = new Item("Chips", 1.5, 0.1);
+        public string AccountNumber { get; private set; }
+        public string AccountHolderName { get; private set; }
+        public string IdentificationNumber { get; private set; }
+        public decimal Balance { get; private set; }
+        public decimal InterestRate { get; private set; }
+        public List<Transaction> Transactions { get; private set; }
 
-        BillLine billLine1 = new BillLine(item1, 2);
-        BillLine billLine2 = new BillLine(item2, 3);
-        BillLine billLine3 = new BillLine(item3, 1);
+        public Account(string accountNumber, string accountHolderName, string identificationNumber, decimal initialBalance, decimal interestRate)
+        {
+            AccountNumber = accountNumber;
+            AccountHolderName = accountHolderName;
+            IdentificationNumber = identificationNumber;
+            Balance = initialBalance;
+            InterestRate = interestRate;
+            Transactions = new List<Transaction>();
+        }
 
-        GroceryBill groceryBill = new GroceryBill(clerk);
-        groceryBill.Add(billLine1);
-        groceryBill.Add(billLine2);
-        groceryBill.Add(billLine3);
+        public void Deposit(decimal amount, DateTime transactionDate)
+        {
+            Balance += amount;
+            Transaction transaction = new Transaction(transactionDate, TransactionType.Nhập, amount);
+            Transactions.Add(transaction);
+        }
 
-        groceryBill.PrintReceipt();
+        public void Withdraw(decimal amount, DateTime transactionDate)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                Transaction transaction = new Transaction(transactionDate, TransactionType.Rút, amount);
+                Transactions.Add(transaction);
+            }
+            else
+            {
+                Console.WriteLine("Số dư không đủ để thực hiện giao dịch.");
+            }
+        }
 
-        Console.WriteLine();
+        public void CalculateInterest()
+        {
+            decimal interest = Balance * InterestRate;
+            Balance += interest;
+        }
+    }
 
-        DiscountBill discountBill = new DiscountBill(clerk, true);
-        discountBill.Add(billLine1);
-        discountBill.Add(billLine2);
-        discountBill.Add(billLine3);
+    // Lớp Transaction (Giao dịch)
+    class Transaction
+    {
+        public DateTime TransactionDate { get; private set; }
+        public TransactionType TransactionType { get; private set; }
+        public decimal Amount { get; private set; }
 
-        discountBill.PrintReceipt();
-        Console.WriteLine($"Số lượng mặt hàng được giảm giá: {discountBill.GetDiscountCount()}");
-        Console.WriteLine($"Tổng số tiền được giảm giá: ${discountBill.GetDiscountAmount()}");
-        Console.WriteLine($"Phần trăm giảm giá: {discountBill.GetDiscountPercent()}%");
+        public Transaction(DateTime transactionDate, TransactionType transactionType, decimal amount)
+        {
+            TransactionDate = transactionDate;
+            TransactionType = transactionType;
+            Amount = amount;
+        }
+    }
+
+    // Enum TransactionType (Kiểu giao dịch)
+    enum TransactionType
+    {
+        Nhập,
+        Rút
+    }
+
+    // Lớp chính (Main class)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding=Encoding.UTF8;
+            Console.OutputEncoding=Encoding.UTF8;
+            Bank bank = new Bank();
+
+            // Mở tài khoản mới
+            bank.OpenAccount("001", "Alice", "901", 100, 0.05m);
+            bank.OpenAccount("002", "Bob", "902", 50, 0.05m);
+            bank.OpenAccount("003", "Alice", "903", 200, 0.05m);
+
+            // Thực hiện giao dịch
+            bank.Deposit("001", 50, DateTime.Now);
+            bank.Withdraw("002", 20, DateTime.Now);
+            bank.Deposit("003", 100, DateTime.Now);
+
+            // Tính lãi suất
+            bank.CalculateInterest();
+
+            // In báo cáo
+            bank.PrintReport();
+        }
     }
 }
